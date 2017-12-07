a6a9d199c78f42aba40e38129f9d724727e800a1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 15:58:42 +0000

    Thu Dec 07 15:58:42 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,14 +3,40 @@ individual_prices = {
     'B': 30,
     'C': 20,
     'D': 15,
+    'E': 40,
 }
+
+class Offer(object):
+    def __init__(self, required, cost):
+        self.required = required
+        self.cost = cost
+
+    def process(self, items, price):
+        for type in individual_prices.keys():
+            if self.required.get(type, 0) > items[type]:
+                return items, price
+
+        for type in individual_prices.keys():
+            items[type] -= self.required.get(type, 0)
+
+        price += self.cost
+
+        return self.process(items, price)
+
+
+offers = [
+    Offer(required={'A': 5}, cost=200),
+    Offer(required={'A': 3}, cost=130),
+    Offer(required={'B': 2}, cost=45),
+    Offer(required={'E': 2, 'B': 1}, cost=80),
+]
 
 
 # noinspection PyUnusedLocal
 def checkout(skus):
     if not isinstance(skus, basestring):
         return -1
-    items = {x: 0 for x in 'ABCD'}
+    items = {x: 0 for x in individual_prices.keys()}
     try:
         for x in skus:
             items[x] += 1
@@ -18,13 +44,8 @@ def checkout(skus):
         return -1
     price = 0
 
-    while items['A'] >= 3:
-        price += 130
-        items['A'] -= 3
-
-    while items['B'] >= 2:
-        price += 45
-        items['B'] -= 2
+    for offer in offers:
+        items, price = offer.process(items, price)
 
     for item, count in items.iteritems():
         price += individual_prices[item] * count
