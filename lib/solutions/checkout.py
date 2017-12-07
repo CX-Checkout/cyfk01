ccf2fd4598a4abacbdafc89f82a2afbe3b32eb11
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 15:34:50 +0000

    Thu Dec 07 15:34:50 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,3 +1,30 @@
+individual_prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
+
+
 # noinspection PyUnusedLocal
 def checkout(skus):
-    raise NotImplementedError()
+    items = {x: 0 for x in 'ABCD'}
+    try:
+        for x in skus:
+            items[x] += 1
+    except KeyError:
+        return -1
+    price = 0
+
+    while items['A'] >= 3:
+        price += 130
+        items['A'] -= 3
+
+    while items['B'] >= 2:
+        price += 45
+        items['A'] -= 2
+
+    for item, count in items.iteritems():
+        price += individual_prices[item] * count
+
+    return price
