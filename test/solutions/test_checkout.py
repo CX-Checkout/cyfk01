a651c1b2024f2c6ef971cc9336df3d642c8d96e9
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 16:00:04 +0000

    Thu Dec 07 16:00:04 GMT 2017

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -20,13 +20,22 @@ class TestSum(unittest.TestCase):
         self.assertEqual(160, checkout('BBABCD'))
 
     def test_error(self):
-        self.assertEqual(-1, checkout('E'))
+        self.assertEqual(-1, checkout('Y'))
 
     def test_other_error(self):
         self.assertEqual(-1, checkout(2))
 
     def test_unicode(self):
         self.assertEqual(130, checkout(u'AAA'))
+
+    def test_big_a_offer(self):
+        self.assertEqual(200, checkout('AAAAA'))
+
+    def test_be_offer(self):
+        self.assertEqual(130, checkout('EEBA'))
+
+    def test_both_a_offer(self):
+        self.assertEqual(410, checkout('AAAAAAAAAB'))
 
 
 if __name__ == '__main__':
